27ad934b322ad9e5a00190e2d06196c8308d77f1
najith97 <najit97@gmail.com>
2019-10-23 11:46:09 -0400

    Project 2 initial files

---
+++ Project-2/task1.py
@@ -0,0 +1,54 @@
+"""
+RANSAC Algorithm Problem
+(Due date: Oct. 23, 3 P.M., 2019)
+The goal of this task is to fit a line to the given points using RANSAC algorithm, and output
+the names of inlier points and outlier points for the line.
+
+Do NOT modify the code provided to you.
+Do NOT use ANY API provided by opencv (cv2) and numpy (np) in your code.
+Do NOT import ANY library (function, module, etc.).
+You can use the library random
+Hint: It is recommended to record the two initial points each time, such that you will Not 
+start from this two points in next iteration.
+"""
+import random
+def solution(input_points, t, d, k):
+    """
+    :param input_points:
+           t: t is the perpendicular distance threshold from a point to a line
+           d: d is the number of nearby points required to assert a model fits well, you may not need this parameter
+           k: k is the number of iteration times
+           Note that, n for line should be 2
+           (more information can be found on the page 90 of slides "Image Features and Matching")
+    :return: inlier_points_name, outlier_points_name
+    inlier_points_name and outlier_points_name is two list, each element of them is str type.
+    For example: If 'a','b' is inlier_points and 'c' is outlier_point.
+    the output should be two lists of ['a', 'b'], ['c'].
+    Note that, these two lists should be non-empty.
+    """
+    # TODO: implement this function.
+    # raise NotImplementedError
+    print(type(input_points[0]))
+
+
+if __name__ == "__main__":
+    input_points = [{'name': 'a', 'value': (0.0, 1.0)}, {'name': 'b', 'value': (2.0, 1.0)},
+                    {'name': 'c', 'value': (3.0, 1.0)}, {'name': 'd', 'value': (0.0, 3.0)},
+                    {'name': 'e', 'value': (1.0, 2.0)}, {'name': 'f', 'value': (1.5, 1.5)},
+                    {'name': 'g', 'value': (1.0, 1.0)}, {'name': 'h', 'value': (1.5, 2.0)}]
+    t = 0.5
+    d = 3
+    k = 100
+    inlier_points_name, outlier_points_name = solution(input_points, t, d, k)  # TODO
+    assert len(inlier_points_name) + len(outlier_points_name) == 8  
+    f = open('./results/task1_result.txt', 'w')
+    f.write('inlier points: ')
+    for inliers in inlier_points_name:
+        f.write(inliers + ',')
+    f.write('\n')
+    f.write('outlier points: ')
+    for outliers in outlier_points_name:
+        f.write(outliers + ',')
+    f.close()
+
+
---
+++ Project-2/task2.py
@@ -0,0 +1,36 @@
+"""
+Image Stitching Problem
+(Due date: Oct. 23, 3 P.M., 2019)
+The goal of this task is to stitch two images of overlap into one image.
+To this end, you need to find feature points of interest in one image, and then find
+the corresponding ones in another image. After this, you can simply stitch the two images
+by aligning the matched feature points.
+For simplicity, the input two images are only clipped along the horizontal direction, which
+means you only need to find the corresponding features in the same rows to achieve image stiching.
+
+Do NOT modify the code provided to you.
+You are allowed use APIs provided by numpy and opencv, except “cv2.findHomography()” and
+APIs that have “stitch”, “Stitch”, “match” or “Match” in their names, e.g., “cv2.BFMatcher()” and
+“cv2.Stitcher.create()”.
+"""
+import cv2
+import numpy as np
+import random
+
+def solution(left_img, right_img):
+    """
+    :param left_img:
+    :param right_img:
+    :return: you need to return the result image which is stitched by left_img and right_img
+    """
+
+
+    raise NotImplementedError
+
+if __name__ == "__main__":
+    left_img = cv2.imread('left.jpg')
+    right_img = cv2.imread('right.jpg')
+    result_image = solution(left_img, right_img)
+    cv2.imwrite('results/task2_result.jpg',result_image)
+
+
