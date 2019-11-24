b384fe503b5d2fdbbf4792459d497d8b30961b6f
najith97 <najit97@gmail.com>
2019-11-24 19:56:57 -0400

    K means clustering with K=2 on lenna.png code and result (after 1 iteration)

--- Project-3/project3/task1.py
+++ Project-3/project3/task1.py
@@ -1,18 +1,3 @@
-"""
-K-Means Segmentation Problem
-(Due date: Nov. 25, 11:59 P.M., 2019)
-The goal of this task is to segment image using k-means clustering.
-
-Do NOT modify the code provided to you.
-Do NOT import ANY library or API besides what has been listed.
-Hint: 
-Please complete all the functions that are labeled with '#to do'. 
-You are allowed to add your own functions if needed.
-You should design you algorithm as fast as possible. To avoid repetitve calculation, you are suggested to depict clustering based on statistic histogram [0,255]. 
-You will be graded based on the total distortion, e.g., sum of distances, the less the better your clustering is.
-"""
-
-
 import utils
 import numpy as np
 import json
@@ -33,6 +18,71 @@ def kmeans(img,k):
             Minimum summation of distance between each pixel and its center.  
     """
     # TODO: implement this function.
+    np.random.seed(42)
+    centroids = list()
+    cluster_label = list()
+    cluster_labels = list()
+    distances = list()
+    old_centroids = list()
+    dist = 0
+    min_dist = 2147483646
+    sum_min_dist = 0
+    final_distance = 2147483645
+    final_dist = 2147483646
+    final_centroids = list()
+    final_cluster_labels = list()
+    unique = np.unique(img)
+    centroids.append(np.random.choice(unique))
+    centroids.append(np.random.choice(unique))
+    j = 0
+    length = 0
+    init_centers = list()
+    for i in range(len(unique)):
+        while(j!=len(unique)):
+            if(i!=j):
+                init_centers.append([unique[i],unique[j]])
+            j = j+1
+        j = i+1
+
+    for init_center in init_centers:
+        centroids[0] = init_center[0]
+        centroids[1] = init_center[1]
+        while(centroids!=old_centroids):
+            old_centroids = centroids[:]
+            for i in img:
+                for j in i:
+                    for center in centroids:
+                        dist = np.sqrt((center - j)**2)
+                        if(dist<min_dist):
+                            min_dist = dist
+                            if(center==centroids[1]):
+                                label = 1
+                            else:
+                                label = 0
+                        elif(dist == min_dist):
+                            min_dist = dist
+                            if(centroids[1]<centroids[0]):
+                                label = 1
+                            else:
+                                label = 0
+                    cluster_label.append(label)
+                    sum_min_dist += min_dist
+                    dist = 0
+                    min_dist = 2147483646
+            final_distance = sum_min_dist
+            sum_min_dist = 0
+            cluster_labels = np.reshape(cluster_label,(img.shape[0],img.shape[1]))
+            cluster_label.clear()
+            centroids[0] = np.mean(np.ma.array(img, mask=(cluster_labels)))
+            centroids[1] = np.mean(np.ma.array(img, mask=np.logical_not(cluster_labels)))
+        if(final_distance<final_dist):
+            for i in range(len(centroids)):
+                centroids[i] = int(round(centroids[i]))
+            final_centroids = centroids
+            final_cluster_labels = cluster_labels
+            final_dist = final_distance
+
+    return final_centroids, final_cluster_labels, final_dist 
 
 
 def visualize(centers,labels):
@@ -44,7 +94,14 @@ def visualize(centers,labels):
     """
     # TODO: implement this function.
 
-     
+    x = np.empty(shape=(img.shape[0],img.shape[1]))
+
+    x[labels==0] = centers[0]
+    x[labels==1] = centers[1]
+
+    return x.astype(np.uint8)
+
+
 if __name__ == "__main__":
     img = utils.read_image('lenna.png')
     k = 2
